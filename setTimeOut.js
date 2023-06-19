30bda84d474b21896859171ac0593dc39bbb99cb
manbodhvikram <manbodhvikram@gmail.com>
2023-06-19 18:04:19 +0530

    setTimeOUt_SetTimeInterval

---
+++ setTimeOut.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+// // Example using setTimeout
+// function timerWithTimeout() {
+   
+//         setTimeout(() => {
+       
+//             console.log(`Count with setTimeout: ${"manbodh vikram sah"}`);
+//           }, 10000);
+        
+    
+  
+// }
+
+// timerWithTimeout();
+
+
+
+function timerWithInterval() {
+    setInterval(() => {
+    //   count++;
+      console.log(`Count with setInterval: ${"manbodh vikram sah"}`);
+    }, 1000);
+  }
+  
+  timerWithInterval();
+  
